601a048d4ec9c40dd98abaf9adc26764e33a4849
Chenyue-Guo <lovesail0712@gmail.com>
2024-12-03 16:43:54 -0500

    Update index.js

--- myapp/static/index.js
+++ myapp/static/index.js
@@ -120,7 +120,7 @@ function performSearch(query) {
 
 // Fetch playlists from the server
 function fetchPlaylists() {
-    fetch('/favlist/')
+    fetch('/userfav/')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok ' + response.statusText);
