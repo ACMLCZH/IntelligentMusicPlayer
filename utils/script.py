44cfa0c949641a417e6d6d57a9c0e9cfb3450748
heaplax <1837035764lwj@gmail.com>
2024-12-03 17:33:28 -0500

    async

--- utils/script.py
+++ utils/script.py
@@ -1,42 +1,51 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import json
-import time
-import requests
+import asyncio
+import aiohttp
 
-# driver = webdriver.Edge()
+# 定义并发限制为16
+CONCURRENT_LIMIT = 16
+semaphore = asyncio.Semaphore(CONCURRENT_LIMIT)
 
-with open('songs.json', 'r', encoding='utf-8') as f: 
-    songs = json.load(f)
-
-def convert_duration_to_minutes(duration_str): 
-    # print("duration_str",duration_str)
+async def convert_duration_to_minutes(duration_str): 
     # 分割字符串，获取分钟和秒 
     minutes, seconds = map(int, duration_str.split(':')) 
-    # 将分钟和秒转换为总分钟数 
-    total_minutes = minutes*60 + seconds
+    # 将分钟和秒转换为总秒数
+    total_minutes = minutes * 60 + seconds
     return int(total_minutes)
 
-for song in songs:
-    # print("!!add song:", song)
-    name, author, album, duration, lyrics, topics, mp3_url, cover_url = song.values()
-    # print("!!!",duration)
-    duration = convert_duration_to_minutes(duration)
-    data = {
-        "name": name,
-        "author": author,
-        "album": album,
-        "duration": duration,
-        "lyrics": '.',
-        "topics": ', '.join(topics),
-        "mp3_url": mp3_url,
-        "cover_url": cover_url,
-    }
-    print("data: ",data)
-    local_url = "http://localhost:8000/song/"
-    local_headers = {
-        'content-type': 'application/json',
-    }
-    local_response = requests.post(local_url, headers=local_headers, json=data)
-    
+async def send_song_data(song, session):
+    async with semaphore:  # 使用信号量限制并发数
+        name, author, album, duration, lyrics, topics, mp3_url, cover_url = song.values()
+        duration = await convert_duration_to_minutes(duration)
+        data = {
+            "name": name,
+            "author": author,
+            "album": album,
+            "duration": duration,
+            "lyrics": '.',
+            "topics": ', '.join(topics),
+            "mp3_url": mp3_url,
+            "cover_url": cover_url,
+        }
+        print("Sending data: ", data)
+        local_url = "http://localhost:8000/song/"
+        headers = {
+            'content-type': 'application/json',
+        }
+        async with session.post(local_url, headers=headers, json=data) as response:
+            return await response.text()
+
+async def main():
+    # 读取 JSON 文件
+    with open('utils/songs.json', 'r', encoding='utf-8') as f: 
+        songs = json.load(f)
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [send_song_data(song, session) for song in songs]
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            print("Response: ", response)
+
+# 运行异步主程序
+if __name__ == '__main__':
+    asyncio.run(main())
