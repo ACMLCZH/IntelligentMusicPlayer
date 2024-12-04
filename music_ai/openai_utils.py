ef21d8d0aa3e1d264afd2536d0c3840ce0349c2d
ACMLCZH <965503066@qq.com>
2024-12-04 01:30:35 -0500

    Update openai_utils.py

--- music_ai/openai_utils.py
+++ music_ai/openai_utils.py
@@ -71,11 +71,31 @@ class PlaylistOrganizer:
         
     async def search_songs_by_name(self, name: str) -> List[Dict]:
         # Stub - will be replaced with actual API call
-        return [song for song in self.playlists if song['title'].lower() == name.lower()]
+        local_response = requests.get(local_url.format(search=name, field='name'), headers=local_headers)
+        print([song for song in self.playlists if song['title'].lower() == name.lower()])
+        print(local_response.json())
+        
+        retrieved_song = local_response.json()
+        return [{
+            'id': song['id'],
+            'title': song['name'],
+            'artist': song['author'],
+            'cover': song['cover_url'],
+            'url': song['mp3_url']
+        } for song in retrieved_song]
     
     async def search_songs_by_genre(self, genre: str) -> List[Dict]:
         # Stub - will be replaced with actual API call 
-        return [song for song in self.playlists if song.get('genre','').lower() == genre.lower()]
+
+        local_response = requests.get(local_url.format(search=genre, field='topics'), headers=local_headers)
+        retrieved_song = local_response.json()
+        return [{
+            'id': song['id'],
+            'title': song['name'],
+            'artist': song['author'],
+            'cover': song['cover_url'],
+            'url': song['mp3_url']
+        } for song in retrieved_song]
 
     def parse_instruction(self, instruction: str) -> Dict:
         """Use GPT to parse the natural language instruction"""
