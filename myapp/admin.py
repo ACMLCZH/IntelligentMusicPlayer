09f417e58d4d7ee7571b8078f5a57e1c52b309a8
jiagengliu02 <3200105174@zju.edu.cn>
2024-12-04 16:12:38 -0500

    user-ljg

--- myapp/admin.py
+++ myapp/admin.py
@@ -8,6 +8,8 @@ from .models import Song
 from .documents import SongDocument
 from django_elasticsearch_dsl.registries import registry
 from elastic_search.methods import *
+import requests
+import aiohttp
 
 class SongAdmin(admin.ModelAdmin):
     list_display = (
@@ -31,23 +33,54 @@ class SongAdmin(admin.ModelAdmin):
     
     def get_search_results(self, request, queryset, search_term):
         if search_term:
-            # print("!!!!!!",search_term)
-            # clear_es_database()
-            hits_total, hits = search_music(search_term, 'all', 0, 10)
-            for hit in hits:
-                print(hit)
-            song_names = [hit['_source']["name"] for hit in hits]
-            queryset = queryset.filter(name__in=song_names)
+            # hits_total, hits = search_music(search_term, 'all', 0, 10)
+            # song_ids = [hit['_source']["id"] for hit in hits]
+
+            url = f"http://localhost:8000/song/search/?search={search_term}&limit=20" 
+            response = requests.get(url) 
+            if response.status_code == 200: 
+                data = response.json()
+            song_ids = [int(item['id']) for item in data['results']]
+            queryset = queryset.filter(id__in=song_ids)
         return super().get_search_results(request, queryset, search_term)
     
-    def save_model(self, request, obj, form, change): 
+    async def save_model(self, request, song, form, change): 
+        add_music(song)
+        data = dict(
+            id=song.id,
+            name=song.name,
+            author=song.author,
+            album=song.album,
+            duration=song.duration,
+            lyrics=song.lyrics,
+            topics=song.topics,
+            mp3_url=song.mp3_url,
+            cover_url=song.cover_url,
+        )
+        print("Sending data: ", data)
+        local_url = "http://localhost:8000/song/"
+        headers = {
+            'content-type': 'application/json',
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(local_url, headers=headers, json=data) as response:
+                return await response.text()
         super().save_model(request, obj, form, change) 
-        add_music(obj)
 
+    # session_id = request.COOKIES.get('sessionid') 
+    # headers = {"Cookie": f"csrftoken={request.COOKIES.get('csrftoken')}; sessionid={session_id}" }
     def delete_queryset(self, request, queryset): 
-        for obj in queryset: 
-            print("music_obj:",obj)
-            delete_music(obj)
+        print("!!!",request)
+        user = request.user 
+        print(dir(request.user))
+        if user.is_authenticated: 
+            for obj in queryset: 
+                print("music_obj:",obj.id)
+                url = f"http://localhost:8000/song/{obj.id}/" 
+                # cookies = { 'csrftoken': str(request.COOKIES.get('csrftoken')), 'sessionid': str(request.COOKIES.get('sessionid')) }
+                response = requests.delete(url, )
+                print("response:",response)
+                return response
         super().delete_model(request, obj)
 admin.site.register(Song, SongAdmin)
 
